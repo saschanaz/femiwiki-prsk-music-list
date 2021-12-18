28e2f39d3d3e9ad41ab773b175cd91993fa7ac49
Kagami Sascha Rosylight <saschanaz@outlook.com>
2021-12-18 11:36:07 +0100

    WikiActionClient

--- wiki.js
+++ wiki.js
@@ -1,31 +1,13 @@
 import config from "./config.json" assert { type: "json" };
-import { wikiRequest } from "./lib/wikiapi.js";
+import { WikiActionClient } from "./lib/wikiapi.js";
 
 // const PAGE_ID = 60469; // 틀:프로세카 악곡 목록
 const PAGE_ID = 60430; // 사용자:사샤나즈/연습장
 
-const {
-  tokens: { logintoken: lgtoken },
-} = await wikiRequest("query", {
-  meta: "tokens",
-  type: "login",
-});
+const client = new WikiActionClient("https://femiwiki.com/w/api.php");
+await client.login("사샤나즈", config.lgname, config.lgpassword);
 
-await wikiRequest("login", {
-  lgname: `사샤나즈@${config.lgname}`,
-  lgpassword: config.lgpassword,
-  lgtoken,
-});
-
-const {
-  tokens: { csrftoken },
-} = await wikiRequest("query", {
-  meta: "tokens",
-  type: "csrf",
-});
-
-await wikiRequest("edit", {
+await client.edit({
   pageid: PAGE_ID,
   text: await Deno.readTextFile("./output.wikitext"),
-  token: csrftoken,
 });
