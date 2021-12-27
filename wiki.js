a87cfb6a69f7d29e4b26d1be6dfe171e427127de
Kagami Sascha Rosylight <saschanaz@outlook.com>
2021-12-28 00:14:16 +0100

    Allow using environment variables instead for gh-actions

--- wiki.js
+++ wiki.js
@@ -1,9 +1,21 @@
-import config from "./config.json" assert { type: "json" };
 import { WikiActionClient } from "./lib/wikiapi.js";
+
+async function getConfig() {
+  try {
+    return (await import("./config.json", { assert: { type: "json" } }))
+      .default;
+  } catch {
+    return {
+      lgname: Deno.env.get("MW_LGNAME"),
+      lgpassword: Deno.env.get("MW_LGPASSWORD"),
+    };
+  }
+}
 
 const PAGE_ID = 60469; // 틀:프로세카 악곡 목록
 // const PAGE_ID = 60430; // 사용자:사샤나즈/연습장
 
+const config = await getConfig();
 const client = new WikiActionClient("https://femiwiki.com/w/api.php");
 await client.login("사샤나즈", config.lgname, config.lgpassword);
 
