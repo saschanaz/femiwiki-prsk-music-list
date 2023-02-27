b074271ed77128fc33c0e6e0ecb034d345c491fa
Kagami Sascha Rosylight <saschanaz@outlook.com>
2023-02-27 09:53:39 +0100

    Migrate to OAuth 2 + REST API

--- wiki.js
+++ wiki.js
@@ -1,4 +1,6 @@
+import { KOR_DATE_FORMAT } from "./lib/utilities.js";
 import { WikiActionClient } from "./lib/wikiapi.js";
+import latest from "./latest.json" assert { type: "json" };
 
 async function getConfig() {
   try {
@@ -6,41 +8,63 @@ async function getConfig() {
       .default;
   } catch {
     return {
-      lgname: Deno.env.get("MW_LGNAME"),
-      lgpassword: Deno.env.get("MW_LGPASSWORD"),
+      access_token: Deno.env.get("MW_ACCESS_TOKEN"),
     };
   }
 }
 
-const MUSICS_PAGE_ID = 60469; // 틀:프로세카 악곡 목록
-const EVENTS_PAGE_ID = 60676; // 틀:프로세카 이벤트 일람
-const GACHAS_PAGE_ID = 61516; // 틀:프로세카 가챠 일람
-// const PAGE_ID = 60430; // 사용자:사샤나즈/연습장
-
 const config = await getConfig();
-const client = new WikiActionClient("https://femiwiki.com/w/api.php");
-await client.login("사샤나즈", config.lgname, config.lgpassword);
+const client = new WikiActionClient(
+  "https://femiwiki.com/rest.php/",
+  config.access_token
+);
 
-await client.edit({
-  pageid: MUSICS_PAGE_ID,
-  text: await Deno.readTextFile(
+const today = KOR_DATE_FORMAT.format(new Date());
+
+const musicsResult = await client.edit("틀:프로세카 악곡 목록", {
+  source: await Deno.readTextFile(
     new URL("./output/musics.wikitext", import.meta.url)
   ),
-  bot: true,
+  comment: `${today} 프로세카봇 악곡 목록 업데이트`,
+  latest: { id: latest.musics },
 });
+console.log(musicsResult);
 
-await client.edit({
-  pageid: EVENTS_PAGE_ID,
-  text: await Deno.readTextFile(
+const eventsResult = await client.edit("틀:프로세카 이벤트 일람", {
+  source: await Deno.readTextFile(
     new URL("./output/events.wikitext", import.meta.url)
   ),
-  bot: true,
+  comment: `${today} 프로세카봇 이벤트 일람 업데이트`,
+  latest: { id: latest.events },
 });
+console.log(eventsResult);
 
-await client.edit({
-  pageid: GACHAS_PAGE_ID,
-  text: await Deno.readTextFile(
+const gachasResult = await client.edit("틀:프로세카 가챠 일람", {
+  source: await Deno.readTextFile(
     new URL("./output/gachas.wikitext", import.meta.url)
   ),
-  bot: true,
+  comment: `${today} 프로세카봇 가챠 일람 업데이트`,
+  latest: { id: latest.gachas },
 });
+console.log(gachasResult);
+
+await Deno.writeTextFile(
+  "latest.json",
+  JSON.stringify(
+    {
+      musics: musicsResult.latest.id,
+      events: eventsResult.latest.id,
+      gachas: gachasResult.latest.id,
+    },
+    null,
+    2
+  ) + "\n"
+);
+
+// await client.edit("사용자:사샤나즈/연습장", {
+//   source: "Hello REST API!",
+//   comment: "foo",
+//   latest: {
+//     id: 227791
+//   }
+// });
